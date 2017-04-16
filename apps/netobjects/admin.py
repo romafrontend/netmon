7e5e1a75a4cbaaddd7fbb59e34d106c465a1e0e0
Landver <good1ni@mail.ru>
2017-04-16 15:48:09 +0300

    created backup app, changes in firewalls

--- apps/netobjects/admin.py
+++ apps/netobjects/admin.py
@@ -6,11 +6,15 @@ from .models import Firewall
 
 class FirewallAdmin(admin.ModelAdmin):
     fieldsets = [
-        ('IP Details',          {'fields': ['ipv4_address', 'port']}),
+        ('IP Details',          {'fields': [
+            'web_address', 'cli_access_port',
+            'cli_address', 'ipv4_external_address',
+            'web_access_port'
+        ]}),
         ('Access Credentials',  {'fields': ['login', 'password']}),
         (None,                  {'fields': ['site_name']}),
     ]
-    list_display = ['site_name', 'ipv4_address', 'port', 'login', 'password']
+    list_display = ['site_name', 'web_address', 'cli_address', 'login', 'password']
 
 
 admin.site.register(Firewall, FirewallAdmin)
--- apps/netobjects/models.py
+++ apps/netobjects/models.py
@@ -6,12 +6,17 @@ from apps.customers.models import Site
 
 
 class Firewall(TimeStampedModel):
-    ipv4_address = models.GenericIPAddressField()
-    port = models.IntegerField(default=None, null=True)
+    # data that we add manually
+    web_address = models.CharField(max_length=29, null=True)  # https:// + 255.255.255.255 + :65550
+    cli_access_port = models.IntegerField(default=22, null=True)
     login = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
-
     site_name = models.ForeignKey(Site)
+
+    # fields that suppose to be fullfilled automatically
+    cli_address = models.CharField(max_length=21, blank=True)  # 255.255.255.255 + :65550
+    ipv4_external_address = models.GenericIPAddressField(blank=True, null=True)
+    web_access_port = models.IntegerField(default=None, blank=True, null=True)
 
     def __str__(self):
         return str(self.site_name)
