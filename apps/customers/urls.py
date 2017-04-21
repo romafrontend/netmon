f7bf44bd25dc643d40134f5b27103a8f4bc6dcd5
Landver <good1ni@mail.ru>
2017-04-21 14:23:02 +0300

    update to gitignore

--- apps/customers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'companies/$', views.companies, name='companies'),
-    url(r'sites/$', views.sites, name='sites')
-]
